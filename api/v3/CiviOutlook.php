a5b7c333a639a291df2901d643e0d1c0fe4b7da4
vajeevan <vajeevan@gmail.com>
2015-03-06 14:33:50 +0000

    following civicrm api v3 standards

--- api/v3/CiviOutlook.php
+++ api/v3/CiviOutlook.php
@@ -65,3 +65,26 @@ function civicrm_api3_civi_outlook_createactivity($params) {
   $result = civicrm_api3('Activity', 'create', $customParams);
   return $result;  
 }
+
+/**
+ * CiviOutlook.GetDomain API
+ *
+ * @param array $params
+ * @return array API result descriptor
+ * @see civicrm_api3_create_success
+ * @see civicrm_api3_create_error
+ * @throws API_Exception
+ */
+function civicrm_api3_civi_outlook_getdomain($params) {
+  $customParams = array(
+  'sequential' => 1,
+  );
+  if (isset($params['key']) && !empty($params['key'])) {
+    $customParams['key'] = $params['key'];
+  }
+  if (isset($params['api_key']) && !empty($params['api_key'])) {
+    $customParams['api_key'] = $params['api_key'];
+  }
+  $result = civicrm_api3('Domain', 'get', $customParams);
+  return $result;
+}
