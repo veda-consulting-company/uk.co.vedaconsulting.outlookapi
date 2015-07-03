907597cca37392c697faec74439da4f55c18a8c9
vajeevan <vajeevan@gmail.com>
2015-07-03 17:23:34 +0100

    Uninstall extension drop tables

---
+++ sql/auto_uninstall.sql
@@ -0,0 +1,12 @@
+-- drop  table
+DROP TABLE IF EXISTS outlook_civicrm_audit;
+
+-- drop table
+DROP TABLE IF EXISTS `outlook_civicrm_user_defaults`;
+
+-- drop  table
+DROP TABLE IF EXISTS `outlook_civicrm_setting`;
+
+
+
+
