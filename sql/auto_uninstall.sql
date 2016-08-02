5b9901ba6cfc0d31ea91d3cc628d2016c90c36bb
Priyanka Karan <priyanka.karan26@gmail.com>
2016-08-02 16:08:56 +0100

    custom data settings for syncing groups

--- sql/auto_uninstall.sql
+++ sql/auto_uninstall.sql
@@ -3,3 +3,12 @@ DROP TABLE IF EXISTS outlook_civicrm_audit;
 
 -- drop table
 DROP TABLE IF EXISTS `outlook_civicrm_user_defaults`;
+
+-- drop custom group for syncing groups
+DELETE FROM civicrm_custom_group WHERE name = 'Outlook_Group_Settings';
+
+-- drop option group for syncing groups
+DELETE  FROM civicrm_option_group WHERE name = 'sync_to_outlook';
+
+-- drop table that stores information for syncing groups
+DROP TABLE IF EXISTS civicrm_value_outlook_group_settings_11;
